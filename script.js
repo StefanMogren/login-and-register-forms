03f0d98c2acbf66786f1b77fe5e340ec423c3e17
StefanMogren <stefan_mogren@hotmail.com>
2025-01-29 13:48:16 +0100

    Created index.html, style.css, and script.js. Added list of things to create

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+Skapa funktionerna
+* validateRegistration()
+* validateLogin()
+
+validateRegistration() ska kontrollera att
+* username minst 6 tecken
+* username får inte redan existera
+* password minst 8 tecken
+* password again samma som password
+* checkboxen är checkad
+
+Om validateRegistration() går igenom;
+* Skicka upp användaren till arrayen "users" 
+* Lagra arrayen "users" i localStorage
+
+
+validateLogin() ska
+* Hämta arrayen "users" från localStorage
+* Kolla ifall inloggningsnamn finns i "users"
+* Kolla ifall lösenordet matchar inloggningsnamnet i "users"
+
+Om validateLogin() går igenom;
+* Dölj inloggningsformuläret
+* Visa ett välkomstmeddelande på skärmen med namnet på användaren
+*/
